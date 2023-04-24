0adb711e95ea91aa3360280ec15d37c8b673b2e4
Marag Modh <107123779+maragmodh@users.noreply.github.com>
2023-04-24 14:37:11 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Pattern 8/pattern-8.cpp
@@ -0,0 +1,41 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+public:
+	
+	void printTriangle(int n) {
+	    // code here
+	    for(int i=n-1;i>=0;i--){
+            //printing space 
+            for(int j=0;j<n-i-1;j++){
+                cout<<" ";
+            }
+            //printing star
+            for(int j=0;j<i*2+1;j++){
+                cout<<"*";
+            }
+           
+            cout<<endl;
+	    }
+	}
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        
+        Solution ob;
+        ob.printTriangle(n);
+    }
+    return 0;
+}
+// } Driver Code Ends
