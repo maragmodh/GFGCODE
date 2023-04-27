5556853c3a4ba49efab7b1d2f2783d7eb5a18867
Marag Modh <107123779+maragmodh@users.noreply.github.com>
2023-04-28 00:59:35 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Pattern 15/pattern-15.cpp
@@ -0,0 +1,36 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    void printTriangle(int n) {
+        // code here
+        for(int i=1; i<=n; i++){
+            char count='A';
+            for(int j=1; j<=n-i+1; j++){
+                cout<<count;
+                count++;
+            }
+            cout<<endl;
+        }
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+
+        Solution ob;
+        ob.printTriangle(n);
+    }
+    return 0;
+}
+// } Driver Code Ends
