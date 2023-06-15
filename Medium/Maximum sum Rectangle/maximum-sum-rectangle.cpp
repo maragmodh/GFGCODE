2640b6455ee8ad0c328d5f88ebbb927353184059
Marag Modh <107123779+maragmodh@users.noreply.github.com>
2023-06-15 14:23:04 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Maximum sum Rectangle/maximum-sum-rectangle.cpp
@@ -0,0 +1,59 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    int kadane(vector<int> &v,int n){
+        int maxsum = INT_MIN;
+        int sum = 0;
+        for(int i = 0;i<n;i++){
+            sum+=v[i];
+            maxsum = max(sum,maxsum);
+            if(sum<0)
+                sum = 0;
+        }
+        return maxsum;
+    }
+    int maximumSumRectangle(int R, int C, vector<vector<int>> M) {
+        // code here
+        //int max = maxsumsubArray(arr[0], arr[0].length);
+        int maxsum = INT_MIN;
+        for(int i = 0;i<R;i++){
+            vector<int>v(C);
+            for(int j=i;j<R;j++){
+                for(int k = 0;k<C;k++){
+                    v[k]+=M[j][k];
+                }
+                int sum = kadane(v,C);
+                if(sum>maxsum){
+                    maxsum = sum;
+                }
+            }
+        }
+        return maxsum;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N, M;
+        cin >> N >> M;
+        vector<vector<int>> v(N, vector<int>(M));
+        for (int i = 0; i < N; i++)
+            for (int j = 0; j < M; j++) cin >> v[i][j];
+        Solution ob;
+        cout << ob.maximumSumRectangle(N, M, v) << "\n";
+    }
+}
+// } Driver Code Ends
