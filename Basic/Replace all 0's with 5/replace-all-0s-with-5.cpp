cba700ba64030b2a8e579b70f82050506bd3d61d
Marag Modh <107123779+maragmodh@users.noreply.github.com>
2023-06-18 18:46:04 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Replace all 0's with 5/replace-all-0s-with-5.cpp
@@ -0,0 +1,31 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+int convertFive(int n);
+
+// Driver program to test above function
+int main() {
+    int T;
+    cin >> T;
+    while (T--) {
+        int n;
+        cin >> n;
+        cout << convertFive(n) << endl;
+    }
+}
+// } Driver Code Ends
+
+
+/*you are required to complete this method*/
+int convertFive(int n) {
+    // Your code here
+   string str = to_string(n);
+     for(int i=0;i<str.size();i++){
+          if(str[i]=='0'){
+              str[i]='5';
+          }
+     }
+     int ans=stoi(str);
+     return ans;
+}
