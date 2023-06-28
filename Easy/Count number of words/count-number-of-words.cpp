4f17f0ae6b708985369f57b9773538c6ecb3ad9e
Marag Modh <107123779+maragmodh@users.noreply.github.com>
2023-06-28 18:44:26 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Count number of words/count-number-of-words.cpp
@@ -0,0 +1,45 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+int countWords(string s);
+int main()
+{
+	int t;
+	cin>>t;
+	cin.ignore();
+	while(t--)
+	    {
+	        string s;
+	        getline(cin,s);
+	        cout<<countWords(s)<<endl;
+	    }
+}
+// } Driver Code Ends
+
+
+//User function Template for C++
+
+int countWords(string s)
+{
+        //code here.
+    int  n = s.length();
+    int temp=0;
+    int c=0;
+    for(int i=0;i<n;i++)
+    {
+         if((s[i] == '\\' && s[i+1] == 'n')||(s[i] == '\\' && s[i+1] == 't'))
+         {
+             temp = 0;
+             i++;             
+         }
+         else if (s[i] == ' ')
+             temp = 0;
+        
+         else if(temp==0  )
+         {
+             c++;
+             temp = 1;
+         }
+    }
+    return c;
+}
