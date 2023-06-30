a901e8d1f48bf7f24208ae5a886dd54956d5e1f8
Marag Modh <107123779+maragmodh@users.noreply.github.com>
2023-06-30 21:50:05 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Merge two sorted linked lists/merge-two-sorted-linked-lists.cpp
@@ -0,0 +1,115 @@
+//{ Driver Code Starts
+#include<iostream>
+using namespace std;
+
+/* Link list Node */
+struct Node
+{
+    int data;
+    struct Node *next;
+    
+    Node(int x)
+    {
+        data = x;
+        next = NULL;
+    }
+};
+
+Node* sortedMerge(struct Node* a, struct Node* b);
+
+/* Function to print Nodes in a given linked list */
+void printList(struct Node *n)
+{
+    while (n!=NULL)
+    {
+        cout << n->data << " ";
+        n = n->next;
+    }
+    cout << endl;
+}
+
+/* Driver program to test above function*/
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n,m;
+        cin>>n>>m;
+
+        int data;
+        cin>>data;
+        struct Node *head1 = new Node(data);
+        struct Node *tail1 = head1;
+        for (int i = 1; i < n; ++i)
+        {
+            cin>>data;
+            tail1->next = new Node(data);
+            tail1 = tail1->next;
+        }
+
+        cin>>data;
+        struct Node *head2 = new Node(data);
+        struct Node *tail2 = head2;
+        for(int i=1; i<m; i++)
+        {
+            cin>>data;
+            tail2->next = new Node(data);
+            tail2 = tail2->next;
+        }
+
+        Node *head = sortedMerge(head1, head2);
+        printList(head);
+    }
+    return 0;
+}
+
+// } Driver Code Ends
+
+
+ 
+
+/* Link list Node
+struct Node {
+  int data;
+  struct Node *next;
+  
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+};
+*/
+//Function to merge two sorted linked list.
+Node* sortedMerge(Node* head1, Node* head2)  
+{  
+    // code here
+    Node* head=new Node(-4);
+    Node* t=head;
+    Node* t1=head1;
+    Node* t2=head2;
+    
+    while(t1!=NULL and t2!=NULL){
+        if(t1 ->data <= t2->data){
+            t->next=t1;
+            t=t1;
+            t1=t1->next;
+            t->next=NULL;
+        }
+        else{
+            t->next=t2;
+            t=t2;
+            t2=t2->next;
+            t->next=NULL;
+        }
+    }
+    if(t1!=NULL){
+        t->next=t1;
+    }
+    if(t2!=NULL){
+        t->next=t2;
+    }
+    return head->next;
+    
+}  
