c1df0919890156825740ad12d0281e2acc1cb72d
Marag Modh <107123779+maragmodh@users.noreply.github.com>
2023-07-09 00:51:05 +0530

    Updated Solution - GfG to GitHub

--- Medium/Stock span problem/stock-span-problem.cpp
+++ Medium/Stock span problem/stock-span-problem.cpp
@@ -15,20 +15,18 @@ class Solution
        // Your code here
        vector<int>ans;
        stack<int>s;
-    //   ans.push_back(1);
        for(int i=0;i<n;i++){
-           while(!s.empty() && price[s.top()]<=price[i]){
-               s.pop();
-           }
-           
-        if(s.empty())
-            ans.push_back(i+1);
-        else 
-            ans.push_back(i-s.top());    
-           
-        s.push(i);
-       }
-       return ans;
+           while(!s.empty() && price[s.top()]<=price[i])
+               s.pop();// Remove the elements from the stack which are smaller than or equal to the current price
+       
+       if(s.empty())
+           ans.push_back(i+1); // If the stack is empty, push the current index + 1 to the answer vector
+       else 
+           ans.push_back(i-s.top()); // Push the top element of the stack to the answer vector
+       
+       s.push(i); // Push the current index to the stack
+    }
+    return ans;
     }
 };
 
