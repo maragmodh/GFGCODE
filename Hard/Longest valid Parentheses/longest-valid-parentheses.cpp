bd26eaa6917a1356d9fc534f98ab11ca5ddbcda7
Marag Modh <107123779+maragmodh@users.noreply.github.com>
2023-07-14 14:21:07 +0530

    Updated Solution - GfG to GitHub

--- Hard/Longest valid Parentheses/longest-valid-parentheses.cpp
+++ Hard/Longest valid Parentheses/longest-valid-parentheses.cpp
@@ -11,18 +11,21 @@ class Solution{
 public:
     int maxLength(string s){
         // code here
-        vector<int> v = {-1};
-        int ans = 0;
-        for (int i = 0; i <s.size(); i++)
-            if (s[i] == '(') 
-                v.push_back(i);
-            else if (v.size() == 1) 
-                v[0] = i;
-            else {
-                v.pop_back();
-                ans = max(ans, i - v.back());
-            }
-        return ans;
+         vector<int> v = {-1};
+          int ans = 0;
+        for (int i = 0; i < s.size(); i++) {
+        if (s[i] == '(') {
+            v.push_back(i);
+        } else if (v.size() == 1) {
+            v[0] = i;
+        } else {
+            v.pop_back();   // If the current character is ')', remove the last index
+            // from the vector (corresponding to '(')
+            ans = max(ans, i - v.back()); // Calculate the length between the current
+            // index and the last remaining index in the vector, and update the maximum length if necessary
+        }
+    }
+    return ans;
     }
 };
 
