842a46751423b4be64590cb3626ac533a8227693
Marag Modh <107123779+maragmodh@users.noreply.github.com>
2023-07-14 11:26:34 +0530

    Updated Solution - GfG to GitHub

--- Medium/Length of the longest substring/length-of-the-longest-substring.cpp
+++ Medium/Length of the longest substring/length-of-the-longest-substring.cpp
@@ -13,17 +13,17 @@ class Solution{
         int ans=0;
         while(j<n){
             v[s[j]]++;
-            if(v[s[j]]==1){
+            if(v[s[j]]==1){  // If the count of s[j] is 1 (first occurrence)
                 ans=max(ans,j-i+1);
                 j++;
             }
-            else{
+            else{// If the count of s[j] is greater than 1 (repeated occurrence)
                 while(v[s[j]]>1){
-                    v[s[i]]--;
-                    i++;
+                    v[s[i]]--;// Decrement the count of character s[i] in the vector v
+                    i++; // Move the left pointer forward
                 }
-                ans=max(ans,j-i+1);
-                j++;
+                ans=max(ans,j-i+1); // Update the maximum length of substring
+                j++; // Move the window forward
             }
         }
         return ans;
