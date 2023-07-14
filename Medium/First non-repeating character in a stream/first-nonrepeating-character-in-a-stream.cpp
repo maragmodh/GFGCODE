98ceaad31f733acbf8b5cb7a3c755e78a6225197
Marag Modh <107123779+maragmodh@users.noreply.github.com>
2023-07-14 16:40:38 +0530

    Updated Solution - GfG to GitHub

--- Medium/First non-repeating character in a stream/first-nonrepeating-character-in-a-stream.cpp
+++ Medium/First non-repeating character in a stream/first-nonrepeating-character-in-a-stream.cpp
@@ -5,26 +5,26 @@ using namespace std;
 // } Driver Code Ends
 class Solution {
 	public:
-		string FirstNonRepeating(string A){
-		    // Code here
-		    //samjvano 6e code
-		    map<char,int>mp;
-		    string ans="";
-		    queue<char>q;
-		    for(int i=0;i<A.length();i++){
-		        mp[A[i]]++;
-		        q.push(A[i]);
-		        while(!q.empty() && mp[q.front()]!=1)
-		            q.pop();
-		        if(!q.empty()&&mp[q.front()]==1)
-		            ans += q.front();
-		        else
-		            ans += '#';
-		    }
-		    
-		   return ans;
-		}
+	string FirstNonRepeating(string A){
+	    map<char, int> mp;    // Map to store character frequencies
+        string ans = "";      // String to store the answer
+        queue<char> q;        // Queue to process characters in a FIFO manner
 
+    for (int i = 0; i < A.length(); i++) {
+        mp[A[i]]++;        // Increment the frequency of the current character
+        q.push(A[i]);      // Push the current character into the queue
+// Remove characters from the front of the queue until the front character has a frequency of 1
+    while (!q.empty() && mp[q.front()] != 1)
+        q.pop();       
+// If the front character has a frequency of 1, it is unique, so add it to the answer
+    if (!q.empty() && mp[q.front()] == 1)
+        ans += q.front();   
+    else // If there are no unique characters in the window, add '#' to the answer
+        ans += '#';    
+}
+
+return ans; 
+}
 };
 
 //{ Driver Code Starts.
