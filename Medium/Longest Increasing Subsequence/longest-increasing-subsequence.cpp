278c45282364d7acd0e2b460a5219a0160e5f0cd
Marag Modh <107123779+maragmodh@users.noreply.github.com>
2023-07-16 00:27:15 +0530

    Updated Solution - GfG to GitHub

--- Medium/Longest Increasing Subsequence/longest-increasing-subsequence.cpp
+++ Medium/Longest Increasing Subsequence/longest-increasing-subsequence.cpp
@@ -6,29 +6,11 @@ using namespace std;
 
 
 class Solution
-// IT IS A DP SOLUTION AND IT IS RIGHT BUT NOT WORKING AFTER 111 TC
 {
     public:
-    //Function to find length of lo11ngest increasing subsequence.
-    // int solvemem(int n,int a[],int curr,int prev, vector<vector<int>>&dp){
-    //     if(curr>=n)
-    //         return 0;
-    //     if(dp[curr][prev+1]!=-1)
-    //         return dp[curr][prev+1];
-    //     //take
-    //     int incl=0,excl=0;
-    //     if(prev==-1 || a[curr]>a[prev] )
-    //         incl = 1 + solvemem(n,a,curr+1,curr,dp);
-    //     excl = solvemem(n,a,curr+1,prev,dp);
-    //     // int maxi = ;
-    //     // dp[curr][prev+1] = maxi;
-    //     return dp[curr][prev+1]=max(incl,excl);
-    // }
-    int longestSubsequence(int n, int a[]){
-    // {
-    //   // your code here
-    //   vector<vector<int>>dp(n,vector<int>(n+1,-1));
-    //   return solvemem(n,a,0,-1,dp);
+    //Function to find length of longest increasing subsequence.
+    int longestSubsequence(int n, int a[])
+    {
         vector<int>temp;
         for(int i=0 ; i<n ; i++){
            int ind=lower_bound(temp.begin(),temp.end(),a[i])-temp.begin();
