bb51fb934897b5b459fcbba842253feddd778e50
Marag Modh <107123779+maragmodh@users.noreply.github.com>
2023-07-15 22:42:51 +0530

    Updated Solution - GfG to GitHub

--- Medium/Circular tour/circular-tour.cpp
+++ Medium/Circular tour/circular-tour.cpp
@@ -26,19 +26,26 @@ class Solution{
     //the complete circle without exhausting its petrol in between.
     int tour(petrolPump p[],int n)
     {
-       //Your code here
-       int ans=0;
-        int sum=0;
+        int ans=0; // Store the starting petrol pump index
+        int sum = 0;    // Store the current petrol balance
+         // Traverse the petrol pumps in a circular manner
         for(int i=0;i<n;i++){
             if(p[i].distance>sum+p[i].petrol){
+// If the distance to the next petrol pump is greater than the current petrol balance,
+// update the starting petrol pump index and reset the petrol balance
                 sum=0;
                 ans=i+1;
             }
             else
+//If the distance can be covered with the current petrol balance, subtract the distance
+// from the petrol balance and add the petrol available at the current pump
                 sum+=p[i].petrol-p[i].distance;
         }
+// Complete the circular traversal to check if it is possible to cover the entire tour
         for(int i=0;i<ans;i++)
+// Add the petrol available at each pump and subtract the distance to the next pump
             sum+=p[i].petrol-p[i].distance;
+// If the final petrol balance is non-negative, it is possible to complete the tour
         if(sum>=0)
             return ans;
         return -1;
