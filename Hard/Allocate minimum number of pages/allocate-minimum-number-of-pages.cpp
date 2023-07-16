2f332b4ef6af242bfdc4d211752aef8dc77b0098
Marag Modh <107123779+maragmodh@users.noreply.github.com>
2023-07-16 21:09:40 +0530

    Updated Solution - GfG to GitHub

--- Hard/Allocate minimum number of pages/allocate-minimum-number-of-pages.cpp
+++ Hard/Allocate minimum number of pages/allocate-minimum-number-of-pages.cpp
@@ -7,48 +7,45 @@ using namespace std;
 
 // } Driver Code Ends
 //User function template in C++
-
 class Solution 
 {
     public:
-    bool istrue(int n,int m,int mid,int A[]){
-        int studentcount = 1;
-        int pagesum=0;
-        for(int i=0;i<n;i++){
-            if(pagesum+A[i]<=mid)
-                pagesum += A[i];
-            else{
-                studentcount++;
-                if(studentcount>m||A[i]>mid)
-                    return false;
-            
-                pagesum=A[i];
-            }
-        }
-        return true;
-    }
-    //Function to find minimum number of pages.
-    int findPages(int A[], int n, int m) 
-    {
-        //code here
-        if(n<m)
-            return -1;
-        int s=0;
-        int sum=0;
-        for(int i=0;i<n;i++)
-            sum += A[i];
-        int e=sum;
-        int ans=-1;
-        int mid = s+(e-s)/2;
-        while(s<=e){
-            if(istrue(n,m,mid,A)){
-                ans = mid;
-                e = mid-1; 
+    bool isPossible(int maxPages, int A[], int M, int N){
+        int children = 1;
+        int pages = 0;
+        for(int i=0; i<N; i++){
+            if(pages + A[i] > maxPages){
+                children++;
+                pages = A[i];
             }
             else
-                s=mid+1;
-                
-            mid = s+(e-s)/2;
+                pages += A[i];
+        }
+        if(children <= M)
+            return true;
+        return false;
+    }
+    int findPages(int A[], int N, int M) 
+    {
+        if(M > N)
+            return -1;
+        int sum = 0;
+        int maxi = INT_MIN;
+        for(int i=0; i<N; i++){
+            sum += A[i];
+            maxi = max(maxi, A[i]);
+        }
+        int i = maxi;
+        int j = sum;
+        int ans = -1;
+        while(i <= j){
+            int mid = i + (j-i)/2;
+            if(isPossible(mid, A, M, N)){
+                ans = mid;
+                j = mid-1;
+            }
+            else
+                i = mid+1;
         }
         return ans;
     }
