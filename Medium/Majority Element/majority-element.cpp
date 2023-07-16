6a76ce08d2780452a97d5524e0a739c8c45703d0
Marag Modh <107123779+maragmodh@users.noreply.github.com>
2023-07-16 23:13:26 +0530

    Updated Solution - GfG to GitHub

--- Medium/Majority Element/majority-element.cpp
+++ Medium/Majority Element/majority-element.cpp
@@ -15,21 +15,16 @@ class Solution{
     // size: size of input array
     int majorityElement(int a[], int size)
     {
-        
-        // your code here
-       unordered_map<int,int>m;
+        unordered_map<int,int>m;
         for(int i=0;i<size;i++)
-        {
             m[a[i]]++;
-        }
         for(int i=0;i<size;i++)
         {
             if(m[a[i]]>(size/2))
-            {
                 return a[i];
-            }
         }
         return -1;
+        
     }
 };
 
