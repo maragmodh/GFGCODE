25c842399e205394834b64774a5e7d723b0f01dd
Marag Modh <107123779+maragmodh@users.noreply.github.com>
2023-07-17 23:32:50 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Consecutive 1's not allowed/consecutive-1s-not-allowed.cpp
@@ -0,0 +1,45 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+#define ll long long
+
+// } Driver Code Ends
+//User function template for C++
+class Solution{
+public:
+	// #define ll long long
+	ll Aditya(int index,vector<int>&dp){
+        if(index<0){
+            return 1;
+        }
+        if(dp[index]!=-1){
+            return dp[index];
+        }
+        int take=Aditya(index-2,dp);
+        int nottake=Aditya(index-1,dp);
+        return dp[index]=(take+nottake)%1000000007;
+    }
+    
+    ll countStrings(int n) {
+        // code here
+        vector<int>dp(n,-1);
+        return Aditya(n-1,dp);
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        Solution ob;
+        auto ans = ob.countStrings(n);
+        cout << ans << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
