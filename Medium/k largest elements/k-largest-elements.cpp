b86d2ccefa2eebd630b2f4fbda16b0b7d17a4cd5
Marag Modh <107123779+maragmodh@users.noreply.github.com>
2023-07-18 21:22:18 +0530

    Updated Solution - GfG to GitHub

--- Medium/k largest elements/k-largest-elements.cpp
+++ Medium/k largest elements/k-largest-elements.cpp
@@ -9,15 +9,22 @@ using namespace std;
 class Solution{
 public:	
 	vector<int> kLargest(int arr[], int n, int k) {
-	    // code here
-	    sort(arr,arr+n);
-        reverse(arr,arr+n);
-        vector<int>ans;
-        for(int i=0;i<k;i++){
-            ans.push_back(arr[i]);
-        }
-        return ans;
-	}
+	     priority_queue<int,vector<int>,greater<int>>q;
+  
+  for(int i=0;i<n;i++)
+  {
+      q.push(arr[i]);
+      if(q.size()>k)
+          q.pop();
+  }
+  vector<int>ans;
+  while(!q.empty()){
+      ans.push_back(q.top());
+      q.pop();
+  }
+ reverse(ans.begin(),ans.end());
+  return ans;
+}
 
 };
 
