24d88a4902143b88148a4cbb59a4172617bc4fdd
Marag Modh <107123779+maragmodh@users.noreply.github.com>
2023-07-18 14:52:14 +0530

    Updated Solution - GfG to GitHub

--- Medium/XOR Game/xor-game.cpp
+++ Medium/XOR Game/xor-game.cpp
@@ -9,6 +9,15 @@ using namespace std;
 
 class Solution{
 public:
+    // Bitwise AND Check: The condition (k & (k+1)) == 0 is used to check
+    // if k and (k+1) have a bitwise AND equal to 0. This condition is
+    // satisfied when k is a power of 2 minus 1.
+
+// Example: For k = 3, (k & (k+1)) evaluates to (3 & 4), which is 0.
+// Example: For k = 7, (k & (k+1)) evaluates to (7 & 8), which is 0.
+// If this condition is true, the function returns k / 2. 
+// This suggests that there is some pattern or relationship between 
+// k and the returned value, specifically when k is a power of 2 minus 1.
     int xorCal(int k){
         if(k==1)
             return 2;
