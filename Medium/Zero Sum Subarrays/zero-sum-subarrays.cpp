9a4175e16ac34ebe3a02b06668241c5ca9f3f4ec
Marag Modh <107123779+maragmodh@users.noreply.github.com>
2023-07-20 00:55:30 +0530

    Updated Solution - GfG to GitHub

--- Medium/Zero Sum Subarrays/zero-sum-subarrays.cpp
+++ Medium/Zero Sum Subarrays/zero-sum-subarrays.cpp
@@ -12,15 +12,13 @@ class Solution{
 public:
     //Function to count subarrays with sum equal to 0.
     long long int findSubarray(vector<long long int> &arr, int n ) {
-        //code here
         map<long long int,int>mp;
         long long sum=0;
         long long ans=0;
         mp[0]=1;
         for(int i=0;i<n;i++){
             sum+=arr[i];
-            if(mp.find(sum)!=mp.end())
-                ans+=mp[sum];
+            ans+=mp[sum];
             mp[sum]++;
         }
         return ans;
