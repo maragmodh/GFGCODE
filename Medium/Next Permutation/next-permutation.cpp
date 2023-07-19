f8f2480041aad261d8df59a508300808c8171190
Marag Modh <107123779+maragmodh@users.noreply.github.com>
2023-07-20 00:36:54 +0530

    Updated Solution - GfG to GitHub

--- Medium/Next Permutation/next-permutation.cpp
+++ Medium/Next Permutation/next-permutation.cpp
@@ -10,9 +10,25 @@ using namespace std;
 class Solution{
 public:
     vector<int> nextPermutation(int N, vector<int> arr){
-        // code here
-        next_permutation(arr.begin(), arr.end());
+        int index = -1;
+        for(int i = N-2; i>= 0; i--){
+            if(arr[i] < arr[i+1]){
+                index = i;
+                break;
+            }
+        }
+        if(index == -1){
+            reverse(arr.begin(), arr.end());
+            return arr;
+        }
         
+        for(int i= N-1; i> index; i--){
+            if(arr[i] > arr[index]){
+                swap(arr[i], arr[index]);
+                break;
+            }
+        }
+        reverse(arr.begin() + index + 1, arr.end());
         return arr;
     }
 };
