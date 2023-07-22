0a6f05bf916a751443b58e88f511a366e6cef44c
Marag Modh <107123779+maragmodh@users.noreply.github.com>
2023-07-22 22:17:54 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/First and last occurrences of x/first-and-last-occurrences-of-x.cpp
@@ -0,0 +1,71 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+int firstOccur(int arr[], int n , int x){
+    int first = -1;
+    int low =0;
+    int high = n-1;
+    while(low <= high){ 
+        int mid = (low+ high) /2;
+        if(arr[mid] == x){
+            first = mid;
+            high = mid -1;
+        }
+        else if(arr[mid] < x)
+            low = mid + 1;
+        else
+            high = mid - 1;
+    }
+    return first;
+}
+int lastOccur(int arr[], int n , int x){
+    int last = -1;
+    int low =0;
+    int high = n-1;
+    while(low <= high){ 
+        int mid = (low+ high) /2;
+        if(arr[mid] == x){
+            last = mid;
+            low = mid + 1;
+        }
+        else if(arr[mid] < x)
+            low = mid + 1;
+        else
+            high = mid - 1;
+    }
+    return last;
+}
+vector<int> find(int arr[], int n , int x )
+{
+   int first = firstOccur(arr, n, x);
+   if(first == -1) return {-1,-1};
+   int last = lastOccur(arr, n, x);
+   return {first,last};
+}
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n,x;
+        cin>>n>>x;
+        int arr[n],i;
+        for(i=0;i<n;i++)
+        cin>>arr[i];
+        vector<int> ans;
+        ans=find(arr,n,x);
+        cout<<ans[0]<<" "<<ans[1]<<endl;
+    }
+    return 0;
+}
+
+
+
+// } Driver Code Ends
