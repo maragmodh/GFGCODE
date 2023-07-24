faa31b3d4d393a5aa44a6aa9ae5516e1f8bdddad
Marag Modh <107123779+maragmodh@users.noreply.github.com>
2023-07-24 21:40:26 +0530

    Updated Solution - GfG to GitHub

--- Easy/Min Number of Flips/min-number-of-flips.cpp
+++ Easy/Min Number of Flips/min-number-of-flips.cpp
@@ -20,18 +20,17 @@ int32_t main()
 
 int minFlips (string S)
 {
-    // your code here
-    int flips=0;
-    for(int i=0;i<S.length();i++){
-        if(i%2==0){
-            if(S[i]=='0')
-                flips++;
-        }
-        else if(i%2!=0){
-            if(S[i]=='1')
-                flips++;
+    int flips = 0; // Counter to keep track of the flips needed
+    for (int i = 0; i < S.length(); i++) {
+        if (i % 2 == 0) { // For even-indexed characters in the string
+            if (S[i] == '1') // If the character is '1'
+                flips++; // Increment the flips counter
+        } else if (i % 2 != 0) { // For odd-indexed characters in the string
+            if (S[i] == '0') // If the character is '0'
+                flips++; // Increment the flips counter
         }
     }
-    int n = S.length()-flips;
-    return min(flips,n);
+
+    int n = S.length() - flips; // Count of characters that need to be flipped to get a pattern like 010101...
+    return min(flips, n); 
 }
