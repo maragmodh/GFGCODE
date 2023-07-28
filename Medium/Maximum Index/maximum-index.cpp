ffcaa513974634e117461c8dd51acfaace8c67fa
Marag Modh <107123779+maragmodh@users.noreply.github.com>
2023-07-28 23:36:35 +0530

    Updated Solution - GfG to GitHub

--- Medium/Maximum Index/maximum-index.cpp
+++ Medium/Maximum Index/maximum-index.cpp
@@ -7,23 +7,25 @@ using namespace std;
 class Solution{
 public:
     int maxIndexDiff(int arr[], int n) {
-        // code here
-        vector < int > v_min(n), v_max(n);
-        v_min[0] = arr[0];
-        v_max[n - 1] = arr[n - 1];
-        for (int i = 1; i < n; i++) {
-        v_min[i] = min(v_min[i - 1], arr[i]);
-        v_max[n - i - 1] = max(v_max[n - i], arr[n - i - 1]);
-         }
-        int ans = 0, i = 0, j = 0;
-        while (i < n && j < n) {
-        if (v_min[i] <= v_max[j]) {
-          ans = max(ans, j - i);
-          j++;
-        } 
-        else i++;
-      }
-      return ans;
+        vector<int> stL;
+        vector<int> stR;
+        for(int i = 0; i<n; i++) {
+            if(stL.empty() || arr[stL.back()]>arr[i]) stL.push_back(i);
+            if(stR.empty() || arr[stR.back()]<arr[n-i-1]) stR.push_back(n-i-1);
+        }
+       
+        int mx = 0;
+        int x = 0;
+        while(!stL.empty() && !stR.empty()) {
+            int i = stL[x];
+            int j = stR.back();
+            if(arr[j] >= arr[i]) {
+                mx = max(mx, j-i);
+                stR.pop_back();
+            } else
+                x++;
+        }
+        return mx;
     }
 
 };
