d4f7d72e63aaae7a30d56806c35e9d8c66962353
Marag Modh <107123779+maragmodh@users.noreply.github.com>
2023-07-31 23:45:50 +0530

    Updated Solution - GfG to GitHub

--- Hard/Reorder List/reorder-list.cpp
+++ Hard/Reorder List/reorder-list.cpp
@@ -35,58 +35,46 @@ struct Node
 
 class Solution{ 
 public:
+    Node* reversell(Node* head){
+       Node* ptr1 = nullptr;
+       Node* ptr2 = nullptr;
+       Node* curr = head;
+
+       while(curr != nullptr){
+           ptr2 = curr->next;
+           curr->next = ptr1;
+           ptr1 = curr;
+           curr = ptr2;
+       }
+       head = ptr1;
+       return head;
+   }
+
     void reorderList(Node* head) {
-        // base condition for a list containing 1 or 2 nodes
-        if(head->next==NULL || head->next->next==NULL)
-        return ;
-        Node * slow=head, *fast=head;
-        // Fast and slow pointer to find mid of the list
-        while(fast->next!=NULL && fast->next->next!=NULL){
-            slow=slow->next;
-            fast=fast->next->next;
-        }
         
-    // if no of nodes are even then this is required to get the correct node
-        // as mid node
-  if(fast->next!=NULL)
-  {
-  slow=slow->next;
-  fast=fast->next;
-  }      
-        // reversing the half linked list
-        if(slow->next!=NULL)
-        fast=slow->next;
-        slow->next=NULL;
-        while(1)
-        {
-            
-          if(fast->next==NULL)
-          {
-              fast->next=slow;
-              break;
-          }
-          else
-          {
-              Node*temp=fast->next;
-              fast->next=slow;
-              slow=fast;
-              fast=temp;
-          }
-            
+        Node* slow = head;
+        Node* fast = head;
+
+        while(fast != NULL && fast->next != NULL){
+            fast = fast->next->next;
+            slow = slow->next;
         }
-        //Final adjusments in the list.
-        slow=head;
-        // 2 additional pointers for manipulation
-        Node *temp1, *temp2;
-        while(fast->next!=NULL)
-        {
-            temp1=slow->next;
-            slow->next=fast;
-            temp2=fast->next;
-            fast->next=temp1;
-            slow=temp1;
-            fast=temp2;
-        }
+
+         Node* temp = reversell(slow->next);
+         slow->next = nullptr;
+
+         Node* ptr1 = head;
+         Node* ptr2 = temp;
+
+         while(ptr1 != nullptr && ptr2 != nullptr){
+
+            Node* ptr3 = ptr1->next;
+            Node* ptr4 = ptr2->next;
+            ptr1->next = ptr2;
+            ptr2->next = ptr3;
+            ptr1 = ptr3;
+            ptr2 = ptr4;
+         }
     }
 };
 
