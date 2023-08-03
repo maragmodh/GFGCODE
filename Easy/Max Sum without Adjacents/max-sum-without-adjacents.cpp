7b2b377706ad5346d41b7f5e9f92ad0dd1c051bb
Marag Modh <107123779+maragmodh@users.noreply.github.com>
2023-08-04 00:13:20 +0530

    Updated Solution - GfG to GitHub

--- Easy/Max Sum without Adjacents/max-sum-without-adjacents.cpp
+++ Easy/Max Sum without Adjacents/max-sum-without-adjacents.cpp
@@ -10,17 +10,22 @@ class Solution{
 public:	
 	// calculate the maximum sum with out adjacent
 	int findMaxSum(int *arr, int n) {
-	   int prev1 = arr[0];
-       int prev2 = 0;
-       for(int i=1;i<n;i++){
-           int inc = prev2 + arr[i];
-           int exc = prev1 + 0;
-           
-           prev2 = prev1;
-           prev1 = max(inc,exc);
-       }
-       
-       return prev1;
+	   vector<int>dp(n);
+	   dp[0] = arr[0];
+	   // Iterate through the array starting from the second element (index 1)
+    for (int i = 1; i < n; i++) {
+        // Calculate the maximum sum including the current element (arr[i])
+        //and the element two positions back (dp[i-2])
+        int incl = dp[i - 2] + arr[i];
+
+        // Calculate the maximum sum excluding the current element (arr[i])
+        //and considering the sum up to the previous element (dp[i-1])
+        int excl = dp[i - 1];
+
+        // Store the maximum of the inclusive and exclusive sums in the DP vector
+        dp[i] = max(incl, excl);
+    }
+     return dp[n - 1];
 	}
 };
 
