e854096647a3b4633338b7ed9304b8570b698c2d
Marag Modh <107123779+maragmodh@users.noreply.github.com>
2023-08-04 00:55:00 +0530

    Updated Solution - GfG to GitHub

--- Easy/Array Operations/array-operations.cpp
+++ Easy/Array Operations/array-operations.cpp
@@ -7,21 +7,21 @@ using namespace std;
 class Solution {
   public:
     int arrayOperations(int n, vector<int> &arr) {
-        // code here
-         int ans=0,c;
-        for(int i=0;i<n;i++){
+        int count=0, nonZero=0;
+        for(int i=0;i<n;i++)
             if(arr[i]==0){
-                for(;i<n;i++){
-                    if(arr[i]!=0)break;
-                    c++;
-                }
-                ans++;
+                if(nonZero) { //there must exist a subarray
+                    count++;
+                    nonZero=0; // one subarray traversed
+               }
             }
-        }
-        if(c==n)return 0;
-        if(ans==0)return -1;
-        if(arr[n-1]!=0)ans++;
-        return ans;
+            else
+                nonZero++;
+        if(nonZero==n)
+            return -1;
+        else if(nonZero)
+            count++;
+        return count;
     }
 };
 
