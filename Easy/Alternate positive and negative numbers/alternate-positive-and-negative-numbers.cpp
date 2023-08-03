41cb71b44da3d6717f39245a69ae1f7f809c9acc
Marag Modh <107123779+maragmodh@users.noreply.github.com>
2023-08-03 18:35:32 +0530

    Updated Solution - GfG to GitHub

--- Easy/Alternate positive and negative numbers/alternate-positive-and-negative-numbers.cpp
+++ Easy/Alternate positive and negative numbers/alternate-positive-and-negative-numbers.cpp
@@ -9,28 +9,25 @@ class Solution{
 public:
 
 	void rearrange(int arr[], int n) {
-	    // code here
-	    vector<int>pos;
-	    vector<int>neg;
-	    for(int i=0;i<n;i++){
-	        if(arr[i]<0){
-	            neg.push_back(arr[i]);
-	        }
-	        else{
-	            pos.push_back(arr[i]);
-	        }
-	    }     
-	    int i=0,j=0,k=0;
-	    int n1 = pos.size();
-	    int n2 = neg.size();
-	    while(i<n1 && j<n2){
-	        arr[k++] = pos[i++];
-	        arr[k++] = neg[j++];
-	    }
-	    while(i<n1)
-	        arr[k++] = pos[i++];
-	    while(j<n2)
-	        arr[k++] = neg[j++];
+	    vector<int>temp1,temp2;
+        for(int i=0;i<n;i++){
+            if(arr[i] >=0)temp1.push_back(arr[i]);
+            else temp2.push_back(arr[i]);
+        }
+        int j=0,k=0,i=0;
+        while(i<n){
+            if(j<temp1.size()){
+            arr[i]=temp1[j];
+            j++;
+            i++;
+          }
+            if(k<temp2.size()){
+                arr[i]=temp2[k];
+                k++;
+                i++;
+            }
+        }
+     
 	}
 };
 
