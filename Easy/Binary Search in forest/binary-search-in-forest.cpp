c0fc08b187b9534a81e10a7b512b80808a8042d8
Marag Modh <107123779+maragmodh@users.noreply.github.com>
2023-08-04 00:41:11 +0530

    Updated Solution - GfG to GitHub

--- Easy/Binary Search in forest/binary-search-in-forest.cpp
+++ Easy/Binary Search in forest/binary-search-in-forest.cpp
@@ -5,41 +5,26 @@ using namespace std;
 // } Driver Code Ends
 class Solution{
     public:
-    int calwood(vector<int>tree,int n,int val){
-        int cnt=0;
-        for(int i=0;i<n;i++)
-            tree[i]=tree[i]-val;
-        for(int i=0;i<n;i++){
-            if(tree[i]>=0){
-                cnt+=tree[i];
-            }
-        }
-        return cnt;
-    }
     int find_height(int tree[], int n, int k)
     {
-        // code here
-        int ans=-1;
-        int s=0;
-        int e=0;
-        for(int i=0;i<n;i++)
-            e=max(e,tree[i]);
-        vector<int>v;
-        for(int i=0;i<n;i++)
-            v.push_back(tree[i]);
-        while(s<=e){
-            int mid=s+(e-s)/2;
-            int totalwood=calwood(v,n,mid);
-            if(totalwood==k){
-                ans=mid;
-                break;
-            }
-            else if(totalwood<k)
-                e=mid-1;
-            else
-                s=mid+1;
+       sort(tree, tree+n);
+    int s = 0, e = tree[n-1];
+    while (s<=e){
+        int mid = s+(e-s)/2;
+        int wood = 0;
+        for (int i = 0; i < n; i++) {
+            if (tree[i]>mid)
+                wood+=tree[i]-mid;
         }
-        return ans;
+        if (wood==k)
+            return mid;
+
+        if (wood>k)
+            s = mid+1;
+        else
+            e = mid-1;
+    }
+    return -1;
     }
 };
 
