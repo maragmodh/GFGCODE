7726d5116acdef08ed8d640b7e07f5170654e409
Marag Modh <107123779+maragmodh@users.noreply.github.com>
2023-08-10 00:27:47 +0530

    Updated Solution - GfG to GitHub

--- Easy/Change Bits/change-bits.cpp
+++ Easy/Change Bits/change-bits.cpp
@@ -12,13 +12,17 @@ class Solution {
     vector<int> changeBits(int N) {
         // code here
         vector<int>v;
+        // Calculate the highest power of 2 that is less than or equal to N
         int m=log2(N);
+        // Calculate the largest binary number with m+1 bits (all bits set to 1)
         int x = pow(2,m+1)-1;
+         // Calculate the difference between N and the largest binary number
         v.push_back(abs(N-x));
         v.push_back(x);
         return v;
     }
 };
+
 
 //{ Driver Code Starts.
 int main() {
