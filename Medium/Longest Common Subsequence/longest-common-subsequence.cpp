cccf4d03e8d5f7d45bb5e665efaf604bd7de797c
Marag Modh <107123779+maragmodh@users.noreply.github.com>
2023-08-10 01:01:17 +0530

    Updated Solution - GfG to GitHub

--- Medium/Longest Common Subsequence/longest-common-subsequence.cpp
+++ Medium/Longest Common Subsequence/longest-common-subsequence.cpp
@@ -9,26 +9,26 @@ using namespace std;
 class Solution
 {
     public:
-    int solve(string& a, string& b, int i, int j, vector<vector<int>>& dp){
-        if(i == a.length() || j == b.length())
-        return 0;
+    int memoization(string &s1,int i,int j,string &s2,vector<vector<int>>&dp){
+        if(i>=s1.size() || j>=s2.size())
+            return 0;
+        if(dp[i][j]!=-1)
+            return dp[i][j];
+        int ans=0;
+        if(s1[i]==s2[j])
+            ans = 1+memoization(s1,i+1,j+1,s2,dp);
+        else
+            ans = max(memoization(s1,i,j+1,s2,dp),memoization(s1,i+1,j,s2,dp));
+            
+        return dp[i][j]=ans;
         
-        if(dp[i][j] != -1)
-        return dp[i][j];
-        
-        int ans = 0;
-        if(a[i] == b[j]){
-            ans = 1 + solve(a,b,i+1,j+1,dp);
-        }
-        else{
-            ans = max(solve(a,b,i+1,j,dp), solve(a,b,i,j+1,dp));
-        }
-        return dp[i][j] = ans;
     }
-    int lcs(int x, int y, string s1, string s2)
-    {   
-        vector<vector<int>> dp(s1.length(), vector<int>(s2.length(), -1));
-        return solve(s1, s2, 0, 0, dp);
+    //Function to find the length of longest common subsequence in two strings.
+    int lcs(int n, int m, string s1, string s2)
+    {
+        // your code here
+        vector<vector<int>>dp(s1.size(),vector<int>(s2.size(),-1));
+        return memoization(s1,0,0,s2,dp);
     }
 };
 
