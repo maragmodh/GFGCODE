35109420013be6ce35578a06eb4d2daba3d82f99
Marag Modh <107123779+maragmodh@users.noreply.github.com>
2023-08-10 00:47:55 +0530

    Updated Solution - GfG to GitHub

--- Medium/Longest Increasing Subsequence/longest-increasing-subsequence.cpp
+++ Medium/Longest Increasing Subsequence/longest-increasing-subsequence.cpp
@@ -12,16 +12,20 @@ class Solution
     int longestSubsequence(int n, int a[])
     {
         vector<int>temp;
-        for(int i=0 ; i<n ; i++){
-           int ind=lower_bound(temp.begin(),temp.end(),a[i])-temp.begin();
-           if(ind>=temp.size())
-               temp.push_back(a[i]);
-           else
-               temp[ind]=a[i];
-       }
+        for (int i = 0; i < n; i++) {
+        // Find the index where 'a[i]' could be inserted while maintaining sorted order
+        int ind = lower_bound(temp.begin(), temp.end(), a[i]) - temp.begin();
+        if(ind >= temp.size())
+            // If 'a[i]' is greater than all elements in 'temp', add it to the end
+            temp.push_back(a[i]);
+        else
+            // Replace the element at 'ind' with 'a[i]'
+            temp[ind] = a[i];
+    }
        return temp.size();
     }
 };
+
 
 //{ Driver Code Starts.
 int main()
