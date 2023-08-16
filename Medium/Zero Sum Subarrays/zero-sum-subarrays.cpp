3cf5aa9049d68870e88ee942f5b156259104ad70
Marag Modh <107123779+maragmodh@users.noreply.github.com>
2023-08-16 15:03:42 +0530

    Updated Solution - GfG to GitHub

--- Medium/Zero Sum Subarrays/zero-sum-subarrays.cpp
+++ Medium/Zero Sum Subarrays/zero-sum-subarrays.cpp
@@ -11,19 +11,24 @@ using namespace std;
 class Solution{
 public:
     //Function to count subarrays with sum equal to 0.
-    long long int findSubarray(vector<long long int> &arr, int n ) {
-        map<long long int,int>mp;
-        long long sum=0;
-        long long ans=0;
-        mp[0]=1;
-        for(int i=0;i<n;i++){
-            sum+=arr[i];
-            ans+=mp[sum];
-            mp[sum]++;
-        }
-        return ans;
+    long long int findSubarray(vector<long long int> &arr, int n) {
+    map<long long int, int>mp;  // Map to store the cumulative sum and its frequency
+    long long sum = 0; // Variable to store the cumulative sum
+    long long ans = 0;// Variable to store the count of subarrays with equal 0-sum
+    
+    mp[0] = 1; // Initialize the frequency of 0-sum as 1, as an empty subarray has sum 0
+    
+    for (int i = 0; i < n; i++) {
+        sum += arr[i];  // Calculate the cumulative sum
+        ans += mp[sum];// Add the frequency of the current cumulative sum to the answer
+        mp[sum]++;//Increment the frequency of the current cumulative sum
     }
+    
+    return ans;     // Return the count of subarrays with equal 0-sum
+}
+
 };
+
 
 //{ Driver Code Starts.
 int main()
