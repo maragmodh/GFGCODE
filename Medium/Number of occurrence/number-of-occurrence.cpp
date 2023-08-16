6005ce3e2778ad8368c103f667b77da366d71ca4
Marag Modh <107123779+maragmodh@users.noreply.github.com>
2023-08-16 14:06:33 +0530

    Updated Solution - GfG to GitHub

--- Medium/Number of occurrence/number-of-occurrence.cpp
+++ Medium/Number of occurrence/number-of-occurrence.cpp
@@ -11,25 +11,17 @@ public:
 	/* if x is present in arr[] then returns the count
 		of occurrences of x, otherwise returns 0. */
 	int count(int arr[], int n, int x) {
-    // Find the lower bound of x in the array using std::lower_bound
-    auto fst = lower_bound(arr, arr + n, x);
-    
-    // If the lower bound is equal to the end of the array, x is not present in
-    // the array So, return 0 as there are no occurrences of x
-    if (fst == arr + n)
-        return 0;
-    
-    // Find the upper bound of x in the array using std::upper_bound
-    auto lst = upper_bound(arr, arr + n, x);
-    
-    // Calculate the first and last index of x in the array
-    int fidx = fst - arr; // First index of x
-    int lidx = lst - arr; // Last index of x
-    
-    // Return the count of occurrences of x in the array (last index - first index)
-    return lidx - fidx;
-}
+	    // code here
+	    auto fst=lower_bound(arr,arr+n,x);
+        // if(fst==arr+n)
+        //     return 0;
+        auto lst=upper_bound(arr,arr+n,x);
+        int fidx=fst-arr;
+        int lidx=lst-arr;
+        return lidx-fidx;
+	}
 };
+
 
 //{ Driver Code Starts.
 
