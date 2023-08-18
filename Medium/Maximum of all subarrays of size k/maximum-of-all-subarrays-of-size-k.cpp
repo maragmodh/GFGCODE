ff82381e37d60c74a21fe8712780272ab00a23c2
Marag Modh <107123779+maragmodh@users.noreply.github.com>
2023-08-18 17:58:54 +0530

    Updated Solution - GfG to GitHub

--- Medium/Maximum of all subarrays of size k/maximum-of-all-subarrays-of-size-k.cpp
+++ Medium/Maximum of all subarrays of size k/maximum-of-all-subarrays-of-size-k.cpp
@@ -12,30 +12,21 @@ class Solution
 {
   public:
     //Function to find maximum of each subarray of size k.
-    vector <int> max_of_subarrays(int *arr, int n, int K)
+    vector <int> max_of_subarrays(int *arr, int n, int k)
     {
-        vector<int> result;
-        deque<int> dq; // Using a double-ended queue to keep track of the maximum element in the current window
-    // Process the first K elements separately to initialize the deque
-    for (int i = 0; i < K; i++) {
-        while (!dq.empty() && arr[i] >= arr[dq.back()])
-            dq.pop_back();
-        dq.push_back(i);
-    }
-    // Process the remaining elements
-    for (int i = K; i < n; i++) {
-        // The front of the deque contains the maximum element for the current window
-        result.push_back(arr[dq.front()]); 
-        // Remove elements outside the current window from the front of the deque
-        while (!dq.empty() && dq.front() <= i - K)
-            dq.pop_front();
-        // Remove elements smaller than the current element from the back of the deque
-        while (!dq.empty() && arr[i] >= arr[dq.back()])
-            dq.pop_back();
-        dq.push_back(i); // Add the current element to the deque
-    }
-    result.push_back(arr[dq.front()]); // Add the maximum element for the last window to the result
-    return result;
+        // your code here
+        vector<int>ans;
+        deque<int>dq;
+        for(int i=0;i<n;i++){
+            while(!dq.empty() && arr[i]>=arr[dq.back()])
+                dq.pop_back();
+            while(!dq.empty() && dq.front()==i-k)
+                dq.pop_front();
+            dq.push_back(i);
+            if(i>=k-1)
+                ans.push_back(arr[dq.front()]);
+        }
+        return ans;
     }
 };
 
