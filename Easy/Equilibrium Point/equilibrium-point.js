fd4edc1b394771f781b37980aa783d6b6d4a988d
Marag Modh <107123779+maragmodh@users.noreply.github.com>
2023-08-24 20:57:51 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Equilibrium Point/equilibrium-point.js
@@ -0,0 +1,69 @@
+//{ Driver Code Starts
+//Initial Template for javascript
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+function main() {
+    let t = parseInt(readLine());
+    let i = 0;
+    for(;i<t;i++)
+    {
+        let n = parseInt(readLine());
+        let a = new Array(n);
+        let input_ar = readLine().split(' ').map(x=>parseInt(x));
+        for(let i=0;i<n;i++)
+            a[i] = input_ar[i];
+        let obj = new Solution();
+        console.log(obj.equilibriumPoint(a, n));
+        
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for javascript
+
+/**
+ * @param {number[]} a
+ * @param {number} n
+ * @returns {number}
+*/
+
+class Solution {
+    // Function to find equilibrium point in the array.
+    equilibriumPoint(arr, n)
+    {
+        let sum = 0;
+        let leftSum = 0;
+        for(let i=0; i<n; i++)
+            sum += arr[i];
+        for(let i=0; i<n; i++){
+            sum -= arr[i];   
+            if(leftSum == sum)
+                return i+1;
+            leftSum += arr[i];
+        }
+        return -1;
+    }
+}
