c42e4e34b6a3d6fe5beafaf89464d3b2863ecbad
Marag Modh <107123779+maragmodh@users.noreply.github.com>
2023-09-05 00:32:26 +0530

    Updated Solution - GfG to GitHub

--- Medium/Consecutive 1's not allowed/consecutive-1s-not-allowed.cpp
+++ Medium/Consecutive 1's not allowed/consecutive-1s-not-allowed.cpp
@@ -10,12 +10,10 @@ class Solution{
 public:
 	// #define ll long long
 	ll Aditya(int index,vector<int>&dp){
-        if(index>=dp.size()){
+        if(index>=dp.size())
             return 1;
-        }
-        if(dp[index]!=-1){
+        if(dp[index]!=-1)
             return dp[index];
-        }
         int take=Aditya(index+2,dp);
         int nottake=Aditya(index+1,dp);
         return dp[index]=(take+nottake)%1000000007;
@@ -27,6 +25,8 @@ public:
         return Aditya(0,dp);
     }
 };
+
+
 
 
 //{ Driver Code Starts.
