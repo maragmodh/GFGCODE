58a6adf907c2ca7b836b1fb1d17627222fae7694
Marag Modh <107123779+maragmodh@users.noreply.github.com>
2023-09-05 22:05:53 +0530

    Updated Solution - GfG to GitHub

--- Basic/Check if two arrays are equal or not/check-if-two-arrays-are-equal-or-not.cpp
+++ Basic/Check if two arrays are equal or not/check-if-two-arrays-are-equal-or-not.cpp
@@ -1,92 +1,55 @@
 //{ Driver Code Starts
-//Initial Template for javascript
-'use strict';
+//Initial function template for C++
 
-process.stdin.resume();
-process.stdin.setEncoding('utf-8');
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long 
 
-let inputString = '';
-let currentLine = 0;
+// } Driver Code Ends
+//User function template for C++
 
-process.stdin.on('data', inputStdin => {
-    inputString += inputStdin;
-});
+class Solution{
+    public:
 
-process.stdin.on('end', _ => {
-    inputString = inputString.trim().split('\n').map(string => {
-        return string.trim();
-    });
-    
-    main();    
-});
-
-function readLine() {
-    return inputString[currentLine++];
-}
-
-function main() {
-    let t = parseInt(readLine());
-    for(let i=0;i<t;i++)
-    {
-        let input_line = readLine().split(' ');
-        let n = parseInt(input_line[0]);
-        
-        let arr = new Array(n);
-        let brr = new Array(n);
-        
-        input_line = readLine().split(' ');
-        for(let i=0;i<n;i++)
-        {
-            arr[i] = parseInt(input_line[i]);
+    //Function to check if two arrays are equal or not.
+    bool check(vector<ll> A, vector<ll> B, int N) {
+        // //code here
+        sort(A.begin(),A.end());
+        sort(B.begin(),B.end());
+        for(int i=0;i<N;i++){
+           if(A[i] != B[i])
+                return false;
         }
-        
-        input_line = readLine().split(' ');
-        for(let i=0;i<n;i++)
-        {
-            brr[i] = parseInt(input_line[i]);
-        }
-        let obj = new Solution();
-        
-        let ans = obj.check(arr, brr, n)
-        console.log(ans?1:0);
+        return true;
     }
+};
+
+
+//{ Driver Code Starts.
+int main()
+ {
+    int t;
+    cin>>t;
+    while(t--) {
+        int n;
+        cin>>n;
+        
+        vector<ll> arr(n,0),brr(n,0);
+        
+        // increase the count of elements in first array
+        for(ll i=0;i<n;i++)
+            cin >> arr[i];
+        
+        
+        // iterate through another array
+        // and decrement the count of elements
+        // in the map in which frequency of elements
+        // is stored for first array
+        for(ll i=0;i<n;i++)
+            cin >> brr[i];
+        Solution ob;
+        cout << ob.check(arr,brr,n) << "\n";
+    }
+	return 0;
 }
 // } Driver Code Ends
-
-
-//User function Template for javascript
-
-
-/**
- * @param {number[]} A
- * @param {number[]} B
- * @param {number} N
- * @returns {boolean}
-*/
-class Solution {
-    //Function to check if two arrays are equal or not.
-    check(A,B,N){
-        const m = new Map();
-for(let i=0; i<N; i++){
-    if(m.has(A[i])){
-        m.set(A[i], m.get(A[i]) + 1);
-    } else {
-        m.set(A[i], 1);
-    }
-}
-for(let i=0; i<N; i++){
-    if(m.has(B[i])){
-        m.set(B[i], m.get(B[i]) - 1);
-    } else {
-        m.set(B[i], -1);
-    }
-}
-for(let [key, value] of m){
-    if(value !== 0){
-        return false;
-    }
-}
-return true;
-    }
-    
-}
