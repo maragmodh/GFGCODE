bc2d6a18802fa01b959af74134bac7d2e7ba37ed
Marag Modh <107123779+maragmodh@users.noreply.github.com>
2023-09-24 19:40:45 +0530

    Updated Solution - GfG to GitHub

--- Medium/Maximize Number of 1's/maximize-number-of-1s.cpp
+++ Medium/Maximize Number of 1's/maximize-number-of-1s.cpp
@@ -9,27 +9,21 @@ class Solution{
 public:
     // m is maximum of number zeroes allowed to flip
     // n is size of array
-    //CPP Sliding window approach
     int findZeroes(int arr[], int n, int m) {
-        // code here
-        int onsCount=0;
-        int left=0;
-        int right=0;
-        int zeroCount=0;
-        while(right<n)
+        int maxConsecutiveOne = 0 , start = 0 , zeroCount = 0;
+        for(int i = 0; i < n; i++)
         {
-            if(arr[right]==0)
-                zeroCount++;
-            if(zeroCount>m)
-            {
-                if(arr[left]==0)
-                    zeroCount--;
-                left++;
-            }
-            onsCount = max(onsCount,right-left+1);
-            right++;
+           if(arr[i] == 0) zeroCount++;
+           while(zeroCount > m)
+           {
+               if(arr[start] == 0)
+                   zeroCount--;
+                   
+               start++;
+           }
+           maxConsecutiveOne = max(maxConsecutiveOne , i-start+1);
         }
-        return onsCount;
+       return maxConsecutiveOne;
     }  
 };
 
